3467c7fe991ce656c6b563db188669ed9f72a838
Arinhakeo <9295@holbertonstudents.com>
2024-07-11 21:22:11 +0000

    ari

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+char *_strncat(char *dest, char *src, int n);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
