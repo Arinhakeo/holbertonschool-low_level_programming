1901c4dd497e1b33e08c8fbcfa1c6f00b0b5fb0e
Arinhakeo <9295@holbertonstudents.com>
2024-07-12 12:05:00 +0000

    ari

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *cap_string(char *);
 char *string_toupper(char *);
