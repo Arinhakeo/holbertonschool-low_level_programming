bae79e53fa26a2d1ed0e01ff251fda330d4f0d8b
Arinhakeo <9295@holbertonstudents.com>
2024-07-15 19:08:57 +0000

    ari

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+* print_dog - Prints a struct dog.
+* @d: Pointer to the struct dog to be printed.
+*
+* Description: This function prints the details of a struct dog.
+*              If an element is NULL, it prints (nil) instead.
+*              If the struct pointer is NULL, it prints nothing.
+*/
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
