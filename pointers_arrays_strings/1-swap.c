6b30f220fdf2def2cc341c3f0615d2131c073fda
Arinhakeo <9295@holbertonstudents.com>
2024-07-17 20:48:06 +0000

    ari

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+* main - check the code
+*
+* Description: This function tests the swap_int
+*
+* Return: Always 0 (Success).
+*/
 void swap_int(int *a, int *b)
 {
 	int swap = *a;
