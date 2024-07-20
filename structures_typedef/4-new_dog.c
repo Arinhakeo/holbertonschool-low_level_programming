5013465beeb5edc25c972e31e0d58aeb9a4cdd08
KEOPHIPHATH Arinhasvath <9295@holbertonstudents.com>
2024-07-20 21:04:12 +0200

    ari

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,52 +3,51 @@
 #include "dog.h"
 
 /**
-* new_dog nouveau chien
-* @name: le nom du chien
-* @age: l'âge du chien
-* @owner: le proprio
+*new_dog nouveau chien
+*@name: le nom du chien
+*@age: l'âge du chien
+*@owner: le proprio
 *
-* Return:vise vers le nouveau chien, ou le bute
+*Return:vise vers le nouveau chien, ou le bute
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    int cleps, proprio, i;
+dog_t *dog;
+int cleps, proprio, i;
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
+dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	return (NULL);
 
-    cleps = 0;
-    while (name[cleps])
-        cleps++;
+cleps = 0;
+	while (name[cleps])
+	cleps++;
 
-    proprio = 0;
-    while (owner[proprio])
-        proprio++;
+proprio = 0;
+	while (owner[proprio])
+	proprio++;
 
-    dog->name = malloc(cleps + 1);
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
+dog->name = malloc(cleps + 1);
+	if (dog->name == NULL)
+{
+free(dog);
+	return (NULL);
+}
 
-    dog->owner = malloc(proprio + 1);
-    if (dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog);
-        return (NULL);
-    }
+dog->owner = malloc(proprio + 1);
+if (dog->owner == NULL)
+		{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+		}
 
-    for (i = 0; i <= cleps; i++)
-        dog->name[i] = name[i];
+for (i = 0; i <= cleps; i++)
+	dog->name[i] = name[i];
     
-    for (i = 0; i <= proprio; i++)
-        dog->owner[i] = owner[i];
+for (i = 0; i <= proprio; i++)
+	dog->owner[i] = owner[i];
+	dog->age = age;
 
-    dog->age = age;
-
-    return (dog);
+return (dog);
 }
