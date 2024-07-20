be6810251305b098d7e252d6b3570c612486e473
Arinhakeo <9295@holbertonstudents.com>
2024-07-20 15:57:48 +0000

    ari

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,46 +1,44 @@
 #include <stdlib.h>
-#include "dog.h"
 #include <string.h>
+#include "dog.h"
+
 /**
-* new_dog - Creates a new dog
-* @name: Name of the dog
-* @age: Age of the dog
-* @owner: Owner of the dog
+* new_dog - crée un nouveau chen
+* @name: le nom du chien
+* @age: l'âge du chien
+* @owner: le propriétair
 *
-* Return: Pointer to the newly created dog, or NULL if the function fails
+* Return:vise vers le nouveau chien, ou le bute
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *new_name, *new_owner;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
+	char *chien, *proprio;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return (NULL);
+	return (NULL);
 
-	new_name = malloc(strlen(name) + 1);
-	if (new_name == NULL)
+	chien = malloc(strlen(name) + 1);
+	if (chien == NULL)
 	{
-		free(dog);
-		return (NULL);
+	free(dog);
+	return (NULL);
 	}
-	strcpy(new_name, name);
+	strcpy(chien, name);
 
-	new_owner = malloc(strlen(owner) + 1);
-	if (new_owner == NULL)
+	proprio = malloc(strlen(owner) + 1);
+	if (proprio == NULL)
 	{
-		free(new_name);
-		free(dog);
-		return (NULL);
+	free(chien);
+	free(dog);
+	return (NULL);
 	}
-	strcpy(new_owner, owner);
+	strcpy(proprio, owner);
 
-	dog->name = new_name;
+	dog->name = chien;
 	dog->age = age;
-	dog->owner = new_owner;
+	dog->owner = proprio;
 
 	return (dog);
 }
