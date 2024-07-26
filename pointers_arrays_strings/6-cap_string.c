92aeb5d1d0138c749c4f3068cfd8d1f20dae025c
Arinhakeo <9295@holbertonstudents.com>
2024-07-26 23:36:49 +0200

    ari

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,39 @@
 #include "main.h"
+
 /**
-* cap_string - Capitalizes all words of a string.
-* @str: The string to be modified.
-*
-* Return: A pointer to the modified string.
-*/
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be modified.
+ *
+ * Return: A pointer to the modified string.
+ */
 char *cap_string(char *str)
 {
-	char *ptr = str;
-	int maj = 1;
+    char *ptr = str;
+    int maj = 1;
 
-	while (*str != '\0')
-	{
-		if (*str >= 'a' && *str <= 'z')
-		{
-			if (maj)
-			{
-				*str = *str - ' ';
-				maj = 0;
-			}
-		}
-		else if (*str == ' ' || *str == '\t' || *str == '\n' || *str == ',' ||
-			*str == ';' || *str == '.' || *str == '!' || *str == '?' ||
-			*str == '"' || *str == '(' || *str == ')' || *str == '{' ||
-			*str == '}')
-		{
-			maj = 1;
-
-		}
-		str++;
-	}
-	return (ptr);
+    while (*str != '\0')
+    {
+        if (*str >= 'a' && *str <= 'z')
+        {
+            if (maj)
+            {
+                /* Convert to uppercase by subtracting 'a' and adding 'A' */
+                *str = *str - 'a' + 'A';
+                maj = 0;  /*Next characters are not the beginning of words*/
+            }
+        }
+        else if (*str == ' ' || *str == '\t' || *str == '\n' || *str == ',' ||
+                 *str == ';' || *str == '.' || *str == '!' || *str == '?' ||
+                 *str == '"' || *str == '(' || *str == ')' || *str == '{' ||
+                 *str == '}')
+        {
+            maj = 1; /*Next letter after these characters should be capitalized*/
+        }
+        else
+        {
+            maj = 0; /*Continue with lowercase letters*/
+        }
+        str++;
+    }
+    return (ptr);
 }
