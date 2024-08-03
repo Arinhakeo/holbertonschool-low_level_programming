c4fb6703c1db644c1541db411f0ad2b291c7a5be
Arinhakeo <9295@holbertonstudents.com>
2024-08-03 20:59:17 +0000

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,28 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+/**
+* read_textfile - Reads a text file and prints it to the POSIX standard output
+* @filename: Name of the file to read
+* @letters: Number of letters it should read and print
+*
+* Return: The actual number of letters it could read and print,
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-FILE *file;/*pointuer pour ld escripteur de fichiers*/
-ssize_t n_read = 0;/*nombre de caracteres lus*/
-int c;/*variable pour stocker le caractere lus*/
+int fd;
+ssize_t n_read = 0;
+char c;
 
-file = fopen(filename, "r");/*ouvre le fichier en lecture*/
-if (!file)
-return (0);/*Si pas d'ouverture affiche 0*/
+if (filename == NULL)
+return (0);
 
-while (letters > 0 && (c = fgetc(file)) != EOF)/*lis les caracteres du fichier et les affiches*/
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+while (letters > 0 && read(fd, &c, 1) == 1)
 {
 if (write(STDOUT_FILENO, &c, 1) != 1)
 {
-fclose(file);
-return (0);/*En cas d'erreur d'écriture, fermer le fichier et retourne*/
+close(fd);
+return (0);
 }
 n_read++;
 letters--;
 }
 
-fclose(file);/*ferme le fichier*/
-
-return (n_read); /*Retourner le nombre de caractères ls*/
+close(fd);
+return (n_read);
 }
