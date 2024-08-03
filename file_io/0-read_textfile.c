aad83d8359374695d93489da4d94d9e8e523bfae
Arinhakeo <9295@holbertonstudents.com>
2024-08-03 20:35:14 +0000

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+/**
+* read_textfile - Reads a text file and prints it to the POSIX standard output
+* @filename: Name of the file to read
+* @letters: Number of letters it should read and print
+* Return: The actual number of letters it could read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 FILE *file;/*pointuer pour ld escripteur de fichiers*/
